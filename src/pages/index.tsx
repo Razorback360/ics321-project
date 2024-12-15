10699ba6bbef27fc6fb1b607ecf11881c9c6b67d
Yokozuna59 <u.yokozuna@gmail.com>
2024-12-15 10:03:20 +0300

    fix link and a error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable react/no-unescaped-entities */
-import type { FC } from 'react';
-import Link from 'next/link';
+import type { FC } from "react";
+import Link from "next/link";
 
 const Home: FC = () => {
   return (
     <div>
       <h1>Welcome to the Train Reservation System</h1>
-      <Link href="/Login">
-        <a>Login</a>
+      <Link href="/Login" className="text-blue-500">
+        Login
       </Link>
     </div>
   );
